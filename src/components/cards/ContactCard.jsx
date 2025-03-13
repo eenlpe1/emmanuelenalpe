cd317b12cfe05c920846abc8e81242b9406938a4
Emmanuel Enalpe III <emmanuelenalpe@gmail.com>
2025-03-13 10:29:04 +0800

    fix: update the website link.

--- src/components/cards/ContactCard.jsx
+++ src/components/cards/ContactCard.jsx
@@ -23,10 +23,10 @@ function ContactCard() {
         <a
           target="_blank"
           className="block p-2 rounded-md bg-foreground/5 border border-foreground/10 hover:bg-foreground/15 transition-colors duration-200"
-          href="https://emmanuelenalpe-portfolio.vercel.app/"
+          href="https://emmanuelenalpe.vercel.app/"
         >
           <p className="text-xs text-foreground/70">Link</p>
-          <p className="text-[13px] font-medium">emmanuelenalpe-portfolio.vercel.app</p>
+          <p className="text-sm font-medium">emmanuelenalpe.vercel.app</p>
         </a>
         <a
           className="block p-2 rounded-md bg-foreground/5 border border-foreground/10 hover:bg-foreground/15 transition-colors duration-200"
